f56cc8fc71cd7253f5fe20c6cbbeb7976e9a53d7
gloxinia106 <gloxinia106@gmail.com>
2023-07-10 18:34:50 +0900

    home section

--- app/components/section/home-section.tsx
+++ app/components/section/home-section.tsx
@@ -1,3 +1,55 @@
+import { useSwiper } from "swiper/react";
+
 export const HomeSection = () => {
-  return <div className="bg-red-500 h-full">Home</div>;
+  const swiper = useSwiper();
+  return (
+    <div className="h-full relative">
+      <img
+        src="/home-section/bg.jpg"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
+      <div className="absolute top-0 left-0 w-full h-full object-cover bg-black opacity-80 -z-10"></div>
+      <div className="flex flex-col items-center text-white mx-10">
+        <div className="mt-32 flex flex-col items-center">
+          <h4 className="text-2xl md:text-4xl font-semibold up-animation">
+            만들고 싶어서 배우는 개발자
+          </h4>
+          <div className="mt-5 up-animation delay-2">
+            <h2 className="text-4xl md:text-7xl font-bold inline">최주환</h2>
+            <span className="text-xl md:text-4xl font-bold">입니다.</span>
+          </div>
+          <p className="mt-10 text-center up-animation text-lg md:text-2xl break-keep delay-3">
+            프론트엔드 개발자를 희망합니다. <br />
+            재밌어 보이는 아이디어를 구현하는 것을 좋아합니다.
+            <br />
+            그 과정에서 버그나 문제가 있으면 끝까지 물고 늘어져 해결하는 것이
+            장점입니다. <br />
+          </p>
+        </div>
+
+        <button
+          onClick={() => {
+            swiper.slideNext();
+          }}
+          className="flex flex-col items-center absolute bottom-[10%] text-white up-animation delay-3"
+        >
+          <div className="mb-3">더 알아보기</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-7 h-7 animate-bounce"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 };
